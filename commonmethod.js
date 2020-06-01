8a844e8ad3bed0f2d1b7e458e1c861b8f1f0110b
skyfoxls <2769547708@qq.com>
2020-06-01 17:57:31 +0800

    修改我的页面

--- commonmethod.js
+++ commonmethod.js
@@ -1,7 +1,7 @@
 function requestmethod(par1,par2,par3,callback) {
     var xhl=new XMLHttpRequest();
 //建立连接
-    xhl.open('par1','https://mockapi.eolinker.com/2ZhGVxjacb39010e6753bd9c02ee803e6e3bfeab6e8007c/ + par2')
+    xhl.open('par1','https://mockapi.eolinker.com/2ZhGVxjacb39010e6753bd9c02ee803e6e3bfeab6e8007c/'+par2)
     //设置头信息，以表单的形式传输
     xhl.setRequestHeader("content-type","application/x-www-form-urlencoded");
     //   向服务端发送请求
